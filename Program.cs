88c31e53b44c5550d7fae637fe754601ea888461
TAFADZWA KASHIRI <st10057559@vcconnect.edu.za>
2023-04-24 15:52:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Areas
+{
+    class Shape
+    {
+        public virtual void Area()
+        {
+            Console.WriteLine("");
+        }
+    }
+    class rectangle : Shape
+    {
+        int b = 12;
+        int h = 6;
+        public override void Area()
+        {
+            Console.WriteLine("The  area of the rectangle is : " + b * h);
+        }
+    }
+    class Triangle : Shape
+    {
+        int b = 12;
+        int h = 6;
+        public override void Area()
+        {
+            Console.WriteLine("The area of the Triangle is : " + b * h / 2);
+        }
+
+    }
+    class Parallellogram : Shape
+    {
+        int b = 8;
+        int h = 10;
+        public override void Area()
+        {
+            Console.WriteLine("The area of the parallellogram is : " + b * h);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape myShape = new Shape();
+            Shape myrectangle = new rectangle();
+            Shape myTriangle = new Triangle();
+            Shape myParallellogram = new Parallellogram();
+
+            myShape.Area();
+            myrectangle.Area();
+            myTriangle.Area();
+            myParallellogram.Area();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
